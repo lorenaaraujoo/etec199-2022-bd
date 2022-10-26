b225902ebe4b80aa557cd2fee2c91206cf1d7b6e
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2022-10-26 07:07:26 -0300

    Delete functionEstoque.sql

--- Escola-Idiomas/functionEstoque.sql
+++
@@ -1,9 +0,0 @@
-USE dbEstoque
-
--- Criar uma função que retorne o dia de semana da venda (no formato segunda, terça, etc) ao lado do código da venda, valor total da venda e sua data.
-
--- Criar uma função que receba o código do cliente e retorne o total de vendas que o cliente já realizou.
-
--- Criar uma função que receba o código de um vendedor e o mês e informe o total de vendas do vendedor no mês informado.
-
--- Criar uma função que usando o bdEstoquediga se o cpfdo cliente é ou não válido.
